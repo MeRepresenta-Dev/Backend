55394fb2a7c013f2a13a715063d28e8fdea90a37
Lucas Soares <lsoares267@gmail.com>
2020-09-25 01:39:39 -0300

    Update app.js

--- MeRepresenta/bin/app.js
+++ MeRepresenta/bin/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({msg: 'ok'}))
+app.get('/', (req, res) => res.json({msg: 'ok!'}))
 
 app.listen(3000, () => console.log('API OK!'));
