8de169a28b702d9a5e4f80b3e695053ba2cce415
NinadHora <62748492+NinadHora@users.noreply.github.com>
2020-10-01 00:39:37 -0300

    create, read, update and delete

---
+++ MeRepresenta/src/models/candidate.js
@@ -0,0 +1,76 @@
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    numberEle:String,
+    password: String,
+    permissionLevel: Number
+ });
+
+ const userModel = mongoose.model('Candidate', userSchema);
+
+ exports.createUser = (userData) => {
+    const user = new User(userData);
+    return user.save();
+};
+
+exports.getById = (req, res) => {
+    UserModel.findById(req.params.userId).then((result) => {
+        res.status(200).send(result);
+    });
+ };
+
+ exports.findById = (id) => {
+    return User.findById(id).then((result) => {
+        result = result.toJSON();
+        delete result._id;
+        delete result.__v;
+        return result;
+    });
+};
+
+exports.patchById = (req, res) => {
+    if (req.body.password){
+        let salt = crypto.randomBytes(16).toString('base64');
+        let hash = crypto.createHmac('sha512', salt).update(req.body.password).digest("base64");
+        req.body.password = salt + "$" + hash;
+    }
+    UserModel.patchUser(req.params.userId, req.body).then((result) => {
+            res.status(204).send({});
+    });
+ };
+
+ exports.patchUser = (id, userData) => {
+    return User.findOneAndUpdate({
+        _id: id
+    }, userData);
+};
+
+exports.list = (req, res) => {
+    let limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
+    let page = 0;
+    if (req.query) {
+        if (req.query.page) {
+            req.query.page = parseInt(req.query.page);
+            page = Number.isInteger(req.query.page) ? req.query.page : 0;
+        }
+    }
+    UserModel.list(limit, page).then((result) => {
+        res.status(200).send(result);
+    })
+ };
+
+ exports.list = (perPage, page) => {
+    return new Promise((resolve, reject) => {
+        User.find()
+            .limit(perPage)
+            .skip(perPage * page)
+            .exec(function (err, users) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(users);
+                }
+            })
+    });
+};
