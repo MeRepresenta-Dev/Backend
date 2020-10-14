4ee12c62b213a4edff29056f6090af3982090855
NinadHora <62748492+NinadHora@users.noreply.github.com>
2020-10-13 22:08:32 -0300

    endpointsms test

--- template/src/app/models/user.js
+++ template/src/app/models/user.js
@@ -110,15 +110,36 @@ exports.list = (perPage, page) => {
       });
   });
 };
-function verifySMS(req, res, next){
-  var token = req.headers['x-access-token'];
-  if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
-  
-  jwt.verify(token, process.env.SECRET, function(err, decoded) {
-    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-    
-    // se tudo estiver ok, salva no request para uso posterior
-    req.userId = decoded.id;
-    next();
+
+function requestVerificationCode(phone) {
+  const url = `${apiBaseUrl}/request-verification-code`;
+  const headers = new Headers();
+  headers.append('Content-Type', defaultHeaders['Content-Type']);
+  const body = JSON.stringify({ phoneNumber: phone });
+  const init = {
+    method: 'POST',
+    mode: 'no-cors',
+    headers,
+    body
+  };
+  const request = new Request(url);
+  return fetch(request, init);
+}
+
+function verifyVerificationCode(code, phoneNumber) {
+  const url = `${apiBaseUrl}/verify-verification-code`;
+  const headers = new Headers();
+  headers.append('Content-Type', defaultHeaders['Content-Type']);
+  const body = JSON.stringify({
+    verificationCode: code,
+    phoneNumber
   });
+  const init = {
+    method: 'POST',
+    mode: 'no-cors',
+    headers,
+    body
+  };
+  const request = new Request(url);
+  return fetch(request, init);
 }
