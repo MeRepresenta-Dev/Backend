92f4bbb4bbf0e0912e812b1d4c9e55ea8802b733
NinadHora <62748492+NinadHora@users.noreply.github.com>
2020-10-16 10:17:21 -0300

    Update UserController

--- template/src/app/controllers/UserController.js
+++ template/src/app/controllers/UserController.js
@@ -3,43 +3,56 @@ const jwt = require('jsonwebtoken');
 const userModel = require('../models/user');
 
 class UserController {
-  async store(req, res) {
-    const user = await userModel.create(req.body);
 
-    user.password = undefined;
+    async store(req, res) {
+        const user = await userModel.create(req.body);
 
-    return res.status(201).json(user);
-  }
+        user.password = undefined;
 
-  async auth(req, res) {
-    const { email, password } = req.body;
-
-    const user = await userModel.findOne({
-      email,
-    });
-
-    if (!(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({
-        error: 'Credenciais invalidas',
-      });
+        return res.status(201).json(user);
     }
 
-    const { _id: id } = user;
+    async auth(req, res) {
+        const { email, password } = req.body;
 
-    const token = jwt.sign(
-      {
-        id,
-      },
-      process.env.JWT_KEY,
-      {
-        expiresIn: '1d',
-      }
-    );
+        const user = await userModel.findOne({
+            email,
+        });
 
-    return res.json({
-      token,
-    });
-  }
+        if (!(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({
+                error: 'Credenciais invalidas',
+            });
+        }
+
+        const { _id: id } = user;
+
+        const token = jwt.sign({
+                id,
+            },
+            process.env.JWT_KEY, {
+                expiresIn: '1d',
+            }
+        );
+
+        return res.json({
+            token,
+        });
+    }
+
+    register(req, res) {
+        const { name, email, password, cpf, telefone, secao, facebook, instagram, twitter } = req.body;
+        bcrypt.hash(password, saltRounds)
+            .then(async(hash) => {
+                await userModel.create({ name, email, password, cpf, telefone, secao, facebook, instagram, twitter: hash }, (err, newUser) => {
+                    if (err) {
+                        console.log(err);
+                        return res.status(400).json({ error: 'O Usuário já existe!' })
+                    }
+
+                    return res.json({ message: 'Usuário foi criado!' })
+                })
+            })
+    }
 }
-
 module.exports = new UserController();
