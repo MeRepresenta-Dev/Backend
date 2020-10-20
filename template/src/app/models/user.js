5749a6a2315433c5b57eccefb23be357bbbb22f1
NinadHora <62748492+NinadHora@users.noreply.github.com>
2020-10-20 10:36:21 -0300

    Update user.js

--- template/src/app/models/user.js
+++ template/src/app/models/user.js
@@ -1,7 +1,21 @@
 const bcrypt = require('bcryptjs');
-const mongoose = require('../../config/mongodb');
+const dynamoose = require('dynamoose');
+const uuid = require('uuid');
 
-const UserSchema = mongoose.Schema({
+dynamoose.aws.config.update({
+    region: 'us-east-1',
+});
+
+dynamoose.local();
+
+const UserSchema = new dynamoose.Schema({
+
+        id: {
+            type: String,
+            hashKey: true,
+            default: uuid.v1(),
+        },
+
         name: {
             type: String,
             required: true,
@@ -54,7 +68,7 @@ UserSchema.pre('save', async function(next) {
     next();
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = dynamoose.model('User', UserSchema);
 module.exports = User;
 
 exports.createUser = (userData) => {
