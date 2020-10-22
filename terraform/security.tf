328984ae7e6a59ee2830b68455e51c1054243100
Lucas Soares <lsoares267@gmail.com>
2020-10-21 22:59:04 -0300

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -2,6 +2,13 @@ resource "aws_security_group" "lb" {
   name        = "${local.app_name}-load-balancer-security-group"
   description = "controls access to the ALB"
   vpc_id      = aws_vpc.this.id
+  
+  ingress {
+        protocol = "tcp"
+        from_port = 443
+        to_port = 443
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
   ingress {
     protocol    = "tcp"
