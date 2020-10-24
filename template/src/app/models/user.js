10556b5e7ecb136e06431e520fd1f0dfdf7d9799
NinadHora <62748492+NinadHora@users.noreply.github.com>
2020-10-24 19:57:28 -0300

    Update user.js

--- template/src/app/models/user.js
+++ template/src/app/models/user.js
@@ -55,7 +55,11 @@ const UserSchema = new dynamoose.Schema({
             required: true,
         },
 
-
+       photo: {
+          type: String,
+          required: true,
+         
+       },
     },
 
     {
